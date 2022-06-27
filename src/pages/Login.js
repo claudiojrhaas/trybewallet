0a7f186b238775cde88ea32d3cbaabbf52771155
Cláudio Haas <claudiohaas.cjh@gmail.com>
2022-06-27 17:16:30 -0300

    Add Login component and login inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="emailUser">
+          E-mail:
+          <input
+            data-testid="email-input"
+            name="emailUser"
+            type="email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            data-testid="password-input"
+            name="password"
+            type="password"
+          />
+        </label>
+        <button
+          type="button"
+          // disabled={}
+          onClick={ console.log('cliquei') }
+        >
+          Enviar
+        </button>
+      </div>
+    );
   }
 }
 
