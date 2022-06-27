558abb74f079bc447f7c1620845c230a383f812b
Cláudio Haas <claudiohaas.cjh@gmail.com>
2022-06-27 18:07:15 -0300

    uptate redux files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Route path="/" component={ Login } />
+    <Switch>
+      <Route exact path="/" component={ Login } />
+    </Switch>
   );
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,10 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE) => state;
+
+export default userReducer;
