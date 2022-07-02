3b4d11dd7c1ceade60a9a839ebe8f6f900fe2d38
Cláudio Haas <claudiohaas.cjh@gmail.com>
2022-07-02 20:49:17 -0300

    feat(Table.js): Add data dynamically

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,14 @@
 import React from 'react';
-
-// Descrição;
-// Tag;
-// Método de pagamento;
-// Valor;
-// Moeda;
-// Câmbio utilizado;
-// Valor convertido;
-// Moeda de conversão;
-// Editar/Excluir.
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends React.Component {
   render() {
+    const { expenses } = this.props;
+
     return (
       <table>
-        Tabela
+        { console.log(expenses) }
         <tr>
           <th>Descrição</th>
           <th>Tag</th>
@@ -26,9 +20,39 @@ class Table extends React.Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        { expenses?.map((data, i) => (
+          <tr key={ i }>
+            <td>{ data.description }</td>
+            <td>{ data.tag }</td>
+            <td>{ data.method }</td>
+            <td>{ Number(data.value).toFixed(2) }</td>
+            <td>
+              { data.currency === 'USD'
+                ? 'Dólar Comercial'
+                : (data.exchangeRates[data.currency].name.split('/')[0]) }
+
+            </td>
+            <td>{ Number(data.exchangeRates[data.currency].ask).toFixed(2) }</td>
+            <td>
+              {
+                Number(data.value * data.exchangeRates[data.currency].ask).toFixed(2)
+              }
+
+            </td>
+            <td>Real</td>
+          </tr>
+        ))}
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
